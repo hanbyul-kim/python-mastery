47c4e36086ac9536a489f21d3442361f42b946e7
Hanbyul Kim <hahanbyul@gmail.com>
2023-07-29 15:24:37 +0900

    Add from_row

--- My Solutions/stock.py
+++ My Solutions/stock.py
@@ -2,10 +2,17 @@
 import csv
 
 class Stock:
+    types = (str, int, float)
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
     def cost(self):
         return self.shares * self.price
 
